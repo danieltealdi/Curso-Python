6d6901cbd84dcbab6042bb5315006522fea52379
Daniel Tealdi <danieltealdi@hotmail.com>
2022-05-02 14:05:08 +0200

    PromoCodigo 1

--- promo_codigo/views.py
+++ promo_codigo/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import PromoCodigo
 
 def validar(request):
-    responseData={
-        'id':4,
-        'name':'Alvaro',
-        'roles':['Admin', 'User'],
-    }
-    return JsonResponse(responseData)
+    codigo=request.GET.get('code')
+    promo_codigo=PromoCodigo.objects.filter(codigo=codigo).first()
+    if promo_codigo is None:
+        return JsonResponse({
+            'status':False},
+            status=404)
+    return JsonResponse({
+            'status':True,
+            'codigo':promo_codigo.codigo,
+            'descuento':promo_codigo.descount,
+    })
+     
